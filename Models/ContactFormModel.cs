6cef2db4b166abf6cb01c3636c632ac0b799d48b
Scot Prince <scotprince+github@gmail.com>
2018-07-25 21:22:42 -1000

    Add files via upload

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tenant.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public string RecapSiteKey { get; set; } = string.Empty;
+    }
+
+    public class RecaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes { get; set; }
+    }
+}
